0ed0f11d3ba076a906594d1e02fa3b7cbeae652e
aather.bscs17seecs@seecs.edu.pk <aather.bscs17seecs@seecs.edu.pk>
2020-06-04 16:17:15 +0500

    adding emit functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
 
-
+const io = socketio(server)
 const port = process.env.PORT
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+const welcomeMessage = 'Welcome'
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', welcomeMessage)
+
+    socket.on('sendMessage', (clientMessage) => {
+        io.emit('message', clientMessage)
+
+    })
+})
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
